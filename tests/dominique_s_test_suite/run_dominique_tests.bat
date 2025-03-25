6a298eeaafe62baf95858d06c9b0343c99934563
mbarbier <mbarbier@ansys.com>
2025-03-25 09:47:54 +0100

    removed relative paths from the test suite

--- tests/dominique_s_test_suite/run_dominique_tests.bat
+++ tests/dominique_s_test_suite/run_dominique_tests.bat
@@ -1,29 +1,29 @@
 
-"Debug\dominic_test.exe" -in ./tests_in/test01.bmp -out ./tests_out/test01.bmp
+"dominic_test.exe" -in ./tests_in/test01.bmp -out ./tests_out/test01.bmp
 CALL :err_handle
-"Debug\dominic_test.exe" -in ./tests_in/test02.bmp -out ./tests_out/test02.bmp
+"dominic_test.exe" -in ./tests_in/test02.bmp -out ./tests_out/test02.bmp
 CALL :err_handle
-"Debug\dominic_test.exe" -in ./tests_in/test03.bmp -out ./tests_out/test03.bmp
+"dominic_test.exe" -in ./tests_in/test03.bmp -out ./tests_out/test03.bmp
 CALL :err_handle
-"Debug\dominic_test.exe" -in ./tests_in/test04.bmp -out ./tests_out/test04.bmp
+"dominic_test.exe" -in ./tests_in/test04.bmp -out ./tests_out/test04.bmp
 CALL :err_handle
-"Debug\dominic_test.exe" -in ./tests_in/test01.gif -out ./tests_out/test01.gif
+"dominic_test.exe" -in ./tests_in/test01.gif -out ./tests_out/test01.gif
 CALL :err_handle
-"Debug\dominic_test.exe" -in ./tests_in/test01.jpg -out ./tests_out/test01.jpg
+"dominic_test.exe" -in ./tests_in/test01.jpg -out ./tests_out/test01.jpg
 CALL :err_handle
-"Debug\dominic_test.exe" -in ./tests_in/test02.jpg -out ./tests_out/test02.jpg
+"dominic_test.exe" -in ./tests_in/test02.jpg -out ./tests_out/test02.jpg
 CALL :err_handle
-"Debug\dominic_test.exe" -in ./tests_in/test01.png -out ./tests_out/test01.png
+"dominic_test.exe" -in ./tests_in/test01.png -out ./tests_out/test01.png
 CALL :err_handle
-"Debug\dominic_test.exe" -in ./tests_in/test01.tif -out ./tests_out/test01.tif
+"dominic_test.exe" -in ./tests_in/test01.tif -out ./tests_out/test01.tif
 CALL :err_handle
-"Debug\dominic_test.exe" -in ./tests_in/test02.tif -out ./tests_out/test02.tif
+"dominic_test.exe" -in ./tests_in/test02.tif -out ./tests_out/test02.tif
 CALL :err_handle
-"Debug\dominic_test.exe" -in ./tests_in/test01.jp2 -out ./tests_out/test01.jp2
+"dominic_test.exe" -in ./tests_in/test01.jp2 -out ./tests_out/test01.jp2
 CALL :err_handle
-"Debug\dominic_test.exe" -in ./tests_in/test01.exr -out ./tests_out/test01.exr
+"dominic_test.exe" -in ./tests_in/test01.exr -out ./tests_out/test01.exr
 CALL :err_handle
-"Debug\dominic_test.exe" -in ./tests_in/test01.hdr -out ./tests_out/test01.hdr
+"dominic_test.exe" -in ./tests_in/test01.hdr -out ./tests_out/test01.hdr
 
 echo "Finished with error code : "
 echo %err_tracker%
